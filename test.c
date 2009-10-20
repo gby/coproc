db066179052a1a5f8b4ffdc48172402d8e32e7c9
Gilad Ben-Yossef <gby@watson.codefidence.com>
2009-10-20 10:06:41 +0200

    Re-licensed under the LGPL v3 and updated copyright notices.

--- test.c
+++ test.c
@@ -1,3 +1,26 @@
+
+/* ****************************************************************************
+ *
+ * © Copyright 2007,2008,2009 Codefidence Ltd. 
+ * All rights reserved unless explictly stated otherwise.
+ *
+ * This file is part of CoProc.
+ *
+ * CoProc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ * 
+ * CoProc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CoProc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -36,8 +59,9 @@ int main (int argc, char ** argv) {
         *test_mem = 'B';
 	printf("PPID: %d\n", getpid());
 
-	pid = coproc_create("very_very_very long test_coproc name", NULL, COPROC_SHARE_FS | COPROC_SHARE_SYSVSEM  | COPROC_DETACHED, \
-		cfunc, test_mem);
+	pid = coproc_create("very_very_very long test_coproc name", NULL, \
+			COPROC_SHARE_FS | COPROC_SHARE_SYSVSEM  | COPROC_DETACHED, \
+			cfunc, test_mem);
 
 	if(pid < 0) abort();
 
